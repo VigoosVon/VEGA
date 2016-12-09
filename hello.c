bd14d3979bd82c05476e4e2e75e957c8d31f02f8
Von <1402912240@qq.com>
2016-12-09 15:43:22 +0800

    hello world

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: 冯伟佳 Vivon
+    > Created Time: Fri 09 Dec 2016 03:41:58 PM CST
+    > Mail:1402912240@qq.com 
+    > University Nanjing Institute of Technology
+    > Addr: B404 building 5, Cui Park,Hongjing Road,Jiangning District, Nanjing, Jiangsu ,China
+    > Http://www.ingenic.cn
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("hello,world!!\n");
+}
